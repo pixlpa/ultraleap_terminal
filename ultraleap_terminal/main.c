006fb8ed25cd1f7392c391c8d666a4a45996bc0b
Andrew Benson <pixlpa@gmail.com>
2023-10-06 09:28:43 -0700

    Initial Commit

---
+++ ultraleap_terminal/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ultraleap_terminal
+//
+//  Created by Andrew Benson on 10/6/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
